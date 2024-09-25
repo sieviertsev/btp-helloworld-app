525a867e70da4a29d346f99ceb54ae9892d2bd46
Mykola Sieviertsev <kolya.sieviertsev@gmail.com>
2024-09-25 14:31:21 +0000

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sap.btp.helloworldui5sm.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapbtp/helloworldui5sm/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
